61ad6066a250c151528bbace6e1b490fa88bf98e
gmhoangdeveloper <gmhoangdeveloper@gmail.com>
2020-10-31 15:59:09 +0700

    react image

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
 
   return (
     <>
+    <img src="https://sites.google.com/site/thietkewebtaihanoi/_/rsrc/1480308136221/kien-thuc-web/tim-kiem-hinh-anh-dep-cho-giao-dien-website/T%C3%ACm%20ki%E1%BA%BFm%20h%C3%ACnh%20%E1%BA%A3nh%20%C4%91%E1%BA%B9p%20cho%20giao%20di%E1%BB%87n%20website-1.jpg"></img>
       <p>{count}</p>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
